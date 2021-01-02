a85200522fff88222eef54468513016619c2a950
Chris Lightfoot-Wild <chris@accu.co.uk>
2021-01-02 11:46:31 +0000

    Added retryable error test.

--- tests/Unit/ClientTest.php
+++ tests/Unit/ClientTest.php
@@ -10,6 +10,7 @@ use GuzzleHttp\Handler\MockHandler;
 use GuzzleHttp\HandlerStack;
 use GuzzleHttp\Psr7\Response;
 use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ResponseInterface;
 
 class ClientTest extends TestCase
 {
@@ -67,6 +68,42 @@ class ClientTest extends TestCase
         $this->expectExceptionMessage('Internal Error');
 
         $client->send(new \Accu\Postmen\Requests\Labels\Retrieve('dummy-label'));
+    }
+
+    public function testRetryableError()
+    {
+        $client = $this->createMockedClient(new MockHandler([
+            new Response(200, [], \GuzzleHttp\json_encode([
+                'meta' => [
+                    'code' => 4101,
+                    'message' => 'Internal Error, please try again.',
+                    'details' => [],
+                    'retryable' => true,
+                ],
+            ])),
+            new Response(200, [], \GuzzleHttp\json_encode([
+                'meta' => [
+                    'code' => 200,
+                    'message' => 'OK',
+                    'details' => [],
+                ],
+            ])),
+        ]));
+
+        $mock = $this->getMockBuilder(\Accu\Postmen\Requests\Labels\Retrieve::class)
+            ->setConstructorArgs(['dummy'])
+            ->setMethods(['hydrateResponse'])
+            ->getMock();
+
+        $mock->method('hydrateResponse')->willReturnArgument(0);
+
+        /** @var ResponseInterface $response */
+        $response = $client->send($mock);
+
+        self::assertInstanceOf(ResponseInterface::class, $response);
+
+        $json = \GuzzleHttp\json_decode($response->getBody()->getContents());
+        self::assertEquals(200, $json->meta->code);
     }
 
     private function createMockedClient(MockHandler $mockHandler): Client
